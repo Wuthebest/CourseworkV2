92571257ad19eddea8e54084e0ad8cccd4bf9f59
Alan Paterson <alanpaterson800@gmail.com>
2016-04-30 16:38:19 +0100

    Initial commit

---
+++ Coursework Javascript.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Alan Paterson on 30/04/2016.
+ */
+
+function Increasefont() {
+    var x = document.getElementById("bodytext");
+    x.style.fontSize = "25px";
+
+}
+function comment() {
+    window.alert("Thanks for the comment");
+}
+
+(function search() {
+    var cx = '016694006590770655735:qwu28tmwnh0';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
